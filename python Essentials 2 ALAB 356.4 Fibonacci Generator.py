ed2eb242276099c1561c83306797d82a4c1b39eb
NoOtherParis <parismorganmarion@gmail.com>
2026-08-06 00:30:02 -0400

    Fibonacci generator code

---
+++ python Essentials 2 ALAB 356.4 Fibonacci Generator.py
@@ -0,0 +1,13 @@
+def gen_fibonacci(n):
+    first = 0
+    second = 1
+
+    for _ in range(n):
+        yield first
+        next_number = first + second
+        first = second
+        second = next_number
+
+
+for number in gen_fibonacci(7):
+    print("Fibonacci Number:", number)
